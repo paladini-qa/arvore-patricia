1265e46f417c3774870b690d1cbb9157fc0bfa15
paladini-qa <paladini.qa@gmail.com>
2025-06-05 08:47:08 -0300

    feat: add bitIndex field to node structure for Patricia Tree implementation

--- Operacoes/0_struct_Arvore.h
+++ Operacoes/0_struct_Arvore.h
@@ -1,10 +1,12 @@
 #ifndef STRUCT_ARVORE_H
 #define STRUCT_ARVORE_H
 
-struct nohPrefixo{
-   int                terminal;
-   struct nohPrefixo* esquerda;
-   struct nohPrefixo* direita;
+struct nohPrefixo
+{
+   int terminal;
+   int bitIndex;
+   struct nohPrefixo *esquerda;
+   struct nohPrefixo *direita;
 };
 
 #endif
